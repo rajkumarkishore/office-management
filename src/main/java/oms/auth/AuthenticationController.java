60bea7c5fe51aa705a54840addcf7fafa2d17307
kishore <rajkumar.kishore@gmail.com>
2017-03-24 00:28:10 +0530

    Added login view, login success & login failure request methods

--- src/main/java/oms/auth/AuthenticationController.java
+++ src/main/java/oms/auth/AuthenticationController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,19 +15,25 @@ public class AuthenticationController {
 	private Logger log = LoggerFactory
 			.getLogger(AuthenticationController.class);
 
+	// load login view
 	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
-	public String initialView() {
-		log.debug("*********** DEBUG ************");
-		log.info("*********** INFO ************");
-		log.warn("*********** WARN ************");
-		log.error("*********** ERROR ************");
+	public String loginView() {
+		log.debug("Login view loaded!");
 		return "auth/login";
 	}
 
-	@RequestMapping(value = { "/login" }, method = RequestMethod.POST)
-	public String authenticate(HttpServletRequest request) {
-		log.debug(request.getParameter("username") + ""
-				+ request.getParameter("password"));
+	// login success
+	@RequestMapping(value = { "/authenticated" }, method = RequestMethod.POST)
+	public String loginSuccess(HttpServletRequest request) {
+		log.debug("Login Success!");
+		return "redirect:welcome";
+	}
+
+	// login error
+	@RequestMapping(value = { "/bad-credentials" }, method = RequestMethod.GET)
+	public String loginError(Model model) {
+		log.debug("Login Failure!");
+		model.addAttribute("error", true);
 		return "auth/login";
 	}
 
