8219ebc3326c2aa1bf1d9788fcd4834761c85111
kishore <rajkumar.kishore@gmail.com>
2017-03-25 00:29:23 +0530

    Added code to validate user from database

--- src/main/java/oms/security/WebSecurityConfig.java
+++ src/main/java/oms/security/WebSecurityConfig.java
@@ -1,16 +1,28 @@
 package oms.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
+	//password encryption class
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	        return new BCryptPasswordEncoder();
+	}
+	 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -23,14 +35,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.logoutUrl("/logout")										// logout url
 				.logoutSuccessUrl("/")										// redirected url after logout
 				.permitAll().and()
-				.csrf().disable();											// csrf disabled 
+				.csrf().disable();											// csrf disabled
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth)
 			throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password")
-				.roles("USER");
+
+		auth.userDetailsService(userDetailsService).passwordEncoder(
+				bCryptPasswordEncoder());
 
 	}
 
